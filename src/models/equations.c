8dcc6b90fb7011f4b7502a787b950c0636e2df3d
Nicolas998 <nicolas.velasquezgiron@gmail.com>
2022-04-07 08:55:14 -0500

    adds the seasonality in the exponent

--- src/models/equations.c
+++ src/models/equations.c
@@ -715,9 +715,9 @@ void TilesModel(double t, const double * const y_i, unsigned int dim, const doub
     //q_in += snowmelt;
 
     //Vertical fluxes-
-    double pow_t = (1.0 - s_l/t_L > 0.0)? pow(1.0 - s_l/t_L,3): 0.0;
+    double pow_t = (1.0 - s_l/t_L > 0.0)? pow(1.0 - s_l/t_L,3*forcing_values[3]): 0.0;
     //double pow_t2 = (4.7 - 3.4*(s_s/1.67) > 0.0)? pow(4.7 - 3.4*(s_s/1.67),0.405): 0.0; // Exp kind of Green y Ampt 
-    double q_pl = k2*99.0*pow_t*s_p*forcing_values[3];
+    double q_pl = k2*99.0*pow_t*s_p;
     double q_ls = k2*ki_fac*s_l;//*forcing_values[3];
     //double q_ls = k2*ki_fac*pow_t2*s_l; //Exp Green y Ampt approach
     double q_pLink = k2*s_p;
