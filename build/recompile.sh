69f5bfd8068beb2839a66a2bb9dd9985fcad3cf3
nicolas998 <nicolas.velasquezgiron@gmail.com>
2024-02-08 22:22:46 -0600

    corrects the temp model, now it uses discharge without issues

--- build/recompile.sh
+++ build/recompile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-# module load zlib/1.2.11_parallel_studio-2017.1
-# module load hdf5/1.8.18_parallel_studio-2017.1
-# module load openmpi/2.0.1_parallel_studio-2017.1  
+module load zlib/1.2.11_parallel_studio-2017.1
+module load hdf5/1.8.18_parallel_studio-2017.1
+module load openmpi/2.0.1_parallel_studio-2017.1  
 
 # module load stack/2021.1 
 # module load zlib/1.2.11_intel-2021.2.0
